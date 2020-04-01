f75951e61d13b43c5ecb0773bdfa4bc6445ac25e
renkaifei <18910891894@163.com>
2020-04-01 21:27:29 +0800

    更新

--- js/components.js
+++ js/components.js
@@ -7,13 +7,19 @@ var superMarket = (function(win,$,superMarket){
 		if(option.className) $(txt).addClass(option.className);
 		if(option.value) $(txt).val(option.value);
 		return txt;	
-	}
+	};
 
 	components.div = function(option){
 		var elem = document.createElement("div");
 		if(option.className) $(elem).addClass(option.className);
+		if(option.text) $(elem).text(option.text);	
 		return elem;	
-	}	
-		
+	};	
+	
+	components.h4 = function(option){
+		var h4 = document.createElement("h4");
+		if(option.text) $(h4).text(option.text);
+		return h4;	
+	};	
 	return superMarket;	
 })(window,jQuery,superMarket ||{});
