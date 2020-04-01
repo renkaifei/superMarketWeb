c8127d9c240b5ca7048f504beed558b981885fcb
renkaifei <18910891894@163.com>
2020-04-01 20:56:05 +0800

    组件更新

--- js/components.js
+++ js/components.js
@@ -1,29 +1,19 @@
 var superMarket = (function(win,$,superMarket){
 	components = superMarket.components = superMarket.components || {};
-	components.weui_cells = function(){
-		var weui_cells = document.createElement("div");
-		$(weui_cells).addClass("weui-cells");
-		return weui_cells;	
-	};
-	components.weui_cell = function(){
-		var weui_cell = document.createElement("div");
-		$(weui_cell).addClass("weui-cell");
-		return weui_cell;	
-	};
-	components.weui_cell__hd = function(){
-		var weui_cell__hd = document.createElement("div");
-		$(weui_cell__hd).addClass("weui-cell__hd");
-		return weui_cell__hd;	
-	};	
-	components.weui_cell__bd = function(){
-		var weui_cell__bd = document.createElement("div");
-		$(weui_cell__bd).addClass("weui-cell__bd");
-		return weui_cell__bd;	
-	};
-	components.weui_cell__ft = function(){
-		var weui_cell__ft = document.createElement("div");
-		$(weui_cell__ft).addClass("weui-cell__ft");
-		return weui_cell__ft;
+
+	components.text = function(option){
+		option = option || {};	
+		var txt = document.createElement("input");
+		if(option.className) $(txt).addClass(option.className);
+		if(option.value) $(txt).val(option.value);
+		return txt;	
 	}
+
+	components.div = function(option){
+		var elem = document.createElement("div");
+		if(option.className) $(elem).addClass(option.className);
+		return elem;	
+	}	
+		
 	return superMarket;	
 })(window,jQuery,superMarket ||{});
